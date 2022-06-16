b403d3e8a4be49d89c408dbf9e4e1786289d719d
completenoob-creator <54887747+completenoob-creator@users.noreply.github.com>
2022-06-16 16:10:54 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
